272764a78a2d0faa133cc62d4fda07fd9b964ddd
Attila <szoftteszt2020@gmail.com>
2021-10-14 13:14:18 +0200

    Led fenyero allitas(PWM)

--- Encoder.ino
+++ Encoder.ino
@@ -3,26 +3,39 @@ int outputCLK = 11;
 int outputDT =12;
 int most=1;
 int ezelott=1;
-int szamlalo=0;
+int szamlalo=50;
+int LED=3;
+int fenyero=0;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Encoder kezelő program!");
   for(int i=11;i<=13;i++) pinMode(i, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+  //digitalWrite(LED,1);
   }
 
 void loop() {
   if (digitalRead(sw)==0) {
-    szamlalo=0;
-    Serial.println(szamlalo);}
+    szamlalo=50;
+    Serial.print("A fényerő: ");
+    Serial.print(szamlalo);
+    Serial.println("%");
+   }
     
   most=digitalRead(outputCLK);
   if (most != ezelott)
   {
-    if(digitalRead(outputDT) != most) { szamlalo--;}
-    else { szamlalo++;}
-    szamlalo=constrain(szamlalo,-10,10);
-    Serial.println(szamlalo);
+    if(digitalRead(outputDT) != most) { szamlalo=szamlalo-10;}
+    else { szamlalo=szamlalo+10;}
+    szamlalo=constrain(szamlalo,0,100);
+    Serial.print("A fényerő: ");
+    Serial.print(szamlalo);
+    Serial.println("%");
+    
   }
   ezelott=most;
+  fenyero=map(szamlalo,0,100,0,255);
+  analogWrite(LED, fenyero);
+  
 }
