fa6e7445d6ae168020e952906bf1727f76914293
Attila <szoftteszt2020@gmail.com>
2021-10-14 11:53:44 +0200

    Tekeres erzekelve

--- Encoder.ino
+++ Encoder.ino
@@ -1,6 +1,9 @@
 int sw=13;
 int outputCLK = 11;
 int outputDT =12;
+int most=1;
+int ezelott=1;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Encoder kezelő program!");
@@ -8,10 +11,10 @@ void setup() {
   }
 
 void loop() {
-  for(int i=11;i<=13;i++) {
-    Serial.print(digitalRead(i));
-    Serial.print("; ");
+  most=digitalRead(outputCLK);
+  if (most != ezelott)
+  {
+    Serial.println("Tekerés van!!!");
   }
-  Serial.println("");
-
+  ezelott=most;
 }
