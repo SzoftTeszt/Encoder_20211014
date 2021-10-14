12508ca177b0f672519678578dd4a9a4bd20ab74
Attila <szoftteszt2020@gmail.com>
2021-10-14 11:35:10 +0200

    Encoder Soros porton

--- Encoder.ino
+++ Encoder.ino
@@ -1,8 +1,17 @@
+int sw=13;
+int outputCLK = 11;
+int outputDT =12;
 void setup() {
- 
-}
+  Serial.begin(9600);
+  Serial.println("Encoder kezelő program!");
+  for(int i=11;i<=13;i++) pinMode(i, INPUT_PULLUP);
+  }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(int i=11;i<=13;i++) {
+    Serial.print(digitalRead(i));
+    Serial.print("; ");
+  }
+  Serial.println("");
 
 }
